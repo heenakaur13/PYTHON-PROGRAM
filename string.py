b38037610fd31bafaad82234a479403182deff98
heenakaur13 <43087253+heenakaur13@users.noreply.github.com>
2018-11-03 10:27:43 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,7 @@
+Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=str('Hello Ves')
+>>> b=str('seV olleH')
+>>> print('the reverse of str is',b)
+the reverse of str is seV olleH
+>>> 
